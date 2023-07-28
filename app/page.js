2d84dc9804e6292f918360a01708ea08ce7b850c
Majomon <mauri.monzon91@gmail.com>
2023-07-28 15:51:23 -0300

    Pidiendo usuarios y renderizando

--- app/page.js
+++ app/page.js
@@ -1,5 +1,28 @@
-function IndexPage() {
-  return <h2>IndexPage</h2>;
+async function fetchUsers() {
+  const response = await fetch("https://reqres.in/api/users");
+  const data = await response.json();
+  return data.data;
+}
+
+async function IndexPage() {
+  const users = await fetchUsers();
+  return (
+    <div>
+      <h1>IndexPage</h1>
+      {/*    <div>{JSON.stringify(users)}</div> */}
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>
+            <div>
+              <h5>{user.id}</h5>
+              <p>{user.email}</p>
+            </div>
+            <img src={user.avatar} alt={user.email} />
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default IndexPage;
